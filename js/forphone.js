c0a93424b17f2416df7da114a5a86f4d77772fa4
secretteb <107752726+secretteb@users.noreply.github.com>
2022-08-27 11:53:20 +0200

    phone redirect

--- js/forphone.js
+++ js/forphone.js
@@ -1,5 +1,5 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    window.location.replace("./phone/index.html");
+    window.location.href = "http://www.w3schools.com";
   }else{
     // document.write("not mobile");
   }
