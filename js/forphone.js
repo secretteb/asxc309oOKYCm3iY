fc20910f9b284b5c232b53ee06ab21dde2443753
secretteb <107752726+secretteb@users.noreply.github.com>
2022-08-27 11:55:32 +0200

    lmao

--- js/forphone.js
+++ js/forphone.js
@@ -1,5 +1,5 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    window.location.href = "http://www.w3schools.com";
+    window.location.href = "https://schummel.software";
   }else{
     // document.write("not mobile");
   }
