5d5950ad387c3de4fbd94eb0db27f622affdf085
secretteb <107752726+secretteb@users.noreply.github.com>
2022-08-27 11:58:00 +0200

    lmfao

--- js/forphone.js
+++ js/forphone.js
@@ -1,5 +1,5 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    window.location.href = "https://schummel.software";
+    window.location.href = "https://kroko.sellix.io/product/6233c717acc90";
   }else{
     // document.write("not mobile");
   }
